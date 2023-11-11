251ca7f96426466964e2ff4cac05eddcb67479ec
Savas Tireng <savast@hotmail.de>
2023-11-11 13:42:57 +0100

    Started code for line drawing

--- scripts/lines.js
+++ scripts/lines.js
@@ -1,5 +1,20 @@
-const drawLine = () => {
-  console.log('line')
-}
+let queue = 1
+const lines = document.querySelectorAll('.line')
+const nextLine = getNextLine(queue, lines)
 
-drawLine()
+console.log('NEXT LINE: ', nextLine)
+
+/* Functions */
+
+/**
+ * @param  {number} queue
+ * @param  {NodeList} lines
+ * @returns {Node} next line in queue
+ */
+function getNextLine(queue, lines) {
+  lines.forEach((line) => {
+    if (line.dataset.lineNr === `${queue}`) {
+      return line
+    }
+  })
+}
