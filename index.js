5b123b661442c7539c54d7349b1c50bac1acdb59
Savas Tireng <savast@hotmail.de>
2023-11-16 14:28:42 +0100

    Fetch refactor

--- index.js
+++ index.js
@@ -1,150 +1,92 @@
-/**
- * API code
- * @author Armin Saderi
- */
+async function startGame() {
+  const lines = document.querySelectorAll('.line')
+  const winContainer = document.querySelector('.message--win')
+  const failContainer = document.querySelector('.message--fail')
+  const keys = document.querySelectorAll('.key')
+  let queue = 1 // There are 10 lines - we need to keep track which one is the next to draw
 
-let letters = getRandomWord()
-console.log('HIER =>', letters)
-
-function getRandomWord() {
-  let letters = []
-  // api call
-  const request = new XMLHttpRequest()
-  /** Parameters of api call
-   * language = deutsch
-   * number of given words = 1
-   * */
-  request.open(
-    'GET',
+  const letters = await fetch(
     'https://random-word-api.herokuapp.com/word?lang=de&number=1'
   )
-  request.onload = function () {
-    //console.log(request);
-    if (request.status === 200) {
-      //console.log(request.responseText);
-      //console.log(request.responseText.replace(/[\[\]"]/g, ''));
+    .then((response) => response.json())
+    .then((data) => Array.from(data[0]))
+    .then((word) => {
+      return word
+    })
+    .catch((error) => console.error(error))
 
-      // sanitize breakets and explanation marks: ["Test"] => Test
-      const randomWord = request.responseText.replace(/[\[\]"]/g, '')
+  console.log(letters)
 
-      // push letter of word into array
-      for (let i = 0; i < randomWord.length; i++) {
-        letters.push(randomWord.charAt(i))
-      }
-      console.log('im onload = ' + letters)
+  // Add key event listeners
+  keys.forEach((key) => {
+    key.addEventListener('click', handleClick)
+  })
+
+  /* Functions */
+
+  function handleClick(event) {
+    // Check if game is already over
+    if (getGameStatus() !== 'goes on') {
+      return
+    }
+
+    // Check if letter was right or wrong
+    const letter = event.target.value
+
+    if (letters.includes(letter) || letters.includes(letter.toLowerCase())) {
+      event.target.classList.add('key--disabled')
+      event.target.classList.add('key--true')
     } else {
-      console.log(
-        'Error ' +
-          request.status +
-          ' = ' +
-          request.statusText +
-          'while fetching ' +
-          request.responseURL
-      )
-      console.log('Trying again...')
-      getRandomWord()
+      event.target.classList.add('key--disabled')
+      event.target.classList.add('key--false')
+      drawNextLine()
+    }
+
+    // Check & update game status
+    switch (getGameStatus()) {
+      case 'goes on':
+        return
+      case 'win':
+        winContainer.classList.add('message--active')
+        return
+      case 'fail':
+        failContainer.classList.add('message--active')
+        return
     }
   }
-  request.send()
-  console.log('Kurz vor Ende der func = ' + letters)
-  return letters
-}
 
-/**
- * Keyboard code
- * @author Mehmet Balkan
- */
+  function drawNextLine() {
+    lines.forEach((line) => {
+      if (line.dataset.lineNr === `${queue}`) {
+        line.classList.add('line--active')
+      }
+    })
 
-const keyboard = document.querySelectorAll('.key')
-console.log('hier der queryselector')
-console.log(keyboard)
-console.log(keyboard.button)
-
-keyboard.forEach((key) => {
-  key.addEventListener('click', function (e) {
-    console.log(e.target.value.toLowerCase())
-  })
-})
-
-/**
- * SVG lines code
- * @author Savas Tireng
- */
-
-/* Program */
-
-const lines = document.querySelectorAll('.line')
-const winContainer = document.querySelector('.message--win')
-const failContainer = document.querySelector('.message--fail')
-let queue = 1 // There are 10 lines - we need to keep track which one is the next to draw
-
-// Add key event listeners
-keyboard.forEach((key) => {
-  key.addEventListener('click', handleClick)
-})
-
-/* Functions */
-
-function handleClick(event) {
-  // Check if game is already over
-  if (getGameStatus() !== 'goes on') {
-    return
+    queue++
   }
 
-  // Check if letter was right or wrong
-  const letter = event.target.value.toLowerCase()
-
-  if (letters.includes(letter)) {
-    event.target.classList.add('key--disabled')
-    event.target.classList.add('key--true')
-  } else {
-    event.target.classList.add('key--disabled')
-    event.target.classList.add('key--false')
-    drawNextLine()
-  }
-
-  // Check & update game status
-  switch (getGameStatus()) {
-    case 'goes on':
-      return
-    case 'win':
-      winContainer.classList.add('message--active')
-      return
-    case 'fail':
-      failContainer.classList.add('message--active')
-      return
-  }
-}
-
-function drawNextLine() {
-  lines.forEach((line) => {
-    if (line.dataset.lineNr === `${queue}`) {
-      line.classList.add('line--active')
+  function getGameStatus() {
+    // Return 'fail' if too many mistakes were made
+    if (queue > 10) {
+      return 'fail'
     }
-  })
 
-  queue++
-}
+    // Return 'win' if all words are found
+    let wordsFound = 0
 
-function getGameStatus() {
-  // Return 'fail' if too many mistakes were made
-  if (queue > 10) {
-    return 'fail'
-  }
+    keys.forEach((key) => {
+      if (key.classList.contains('key--true')) {
+        wordsFound++
+      }
+    })
 
-  // Return 'win' if all words are found
-  let wordsFound = 0
-
-  keyboard.forEach((key) => {
-    if (key.classList.contains('key--true')) {
-      wordsFound++
+    if (letters.length === wordsFound) {
+      return 'win'
     }
-  })
 
-  if (letters.length === wordsFound) {
-    return 'win'
+    // Return 'goes on' if no win and no loss yet
+    return 'goes on'
   }
-
-  // Return 'goes on' if no win and no loss yet
-  return 'goes on'
 }
+
+startGame()
